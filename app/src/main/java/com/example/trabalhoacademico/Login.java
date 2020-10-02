fe01451d9da258fb7803acf8a0e9169a47139b50
Danilo <danilo3275@yahoo.com.br>
2020-10-01 22:32:36 -0300

    Adidcionado um "padrão de tela de Login.java"

---
+++ app/src/main/java/com/example/trabalhoacademico/Login.java
@@ -0,0 +1,34 @@
+package com.example.trabalhoacademico;
+
+import java.io.Serializable;
+
+public class Login implements Serializable {
+    private String Login, Senha;
+
+    public Login(String login, String senha) {
+        Login = login;
+        Senha = senha;
+    }
+
+    public Login(){
+        Login=null;
+        Senha=null;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+
+}
